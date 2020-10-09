cf8907a82f6e8f9f8670a55429b52324ecf71c2c
Jital Soni <jsoni@uncc.edu>
2020-10-09 15:22:24 -0400

    cors

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+// Budget API
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
 
 const budget = require("./data_");
 console.log(budget);
 
-app.get ('/hello', (req, res) => {
-    res.send('Hello World!');
-});
+
 
 app.get('/budget',(req, res) => {
     res.json(budget);
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`API app listening at http://localhost:${port}`);
 });
